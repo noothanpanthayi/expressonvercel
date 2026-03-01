cccce1ba1c0fb08f489d3d0ee8f6b96348c9c403
noothanpanthayi <noothankrishnan@outlook.com>
2026-02-28 22:39:06 -0600

    first commit

---
+++ api/server.js
@@ -0,0 +1,9 @@
+// api/index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
+
+module.exports = app; // Export the app instance
