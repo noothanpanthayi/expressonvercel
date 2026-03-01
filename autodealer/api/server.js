1bfe0bad414ab399ca988905b46a3cea93c85ce7
noothanpanthayi <noothankrishnan@outlook.com>
2026-03-01 08:46:06 -0600

    udpates

--- autodealer/api/server.js
+++ autodealer/api/server.js
@@ -14,3 +14,17 @@ app.get('/api/test', (req, res) => {
 });
 
 module.exports = serverless(app);
+
+// const express = require('express');
+// // const serverless = require('serverless-http');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Hello from Express on Vercel!' });
+// });
+
+// app.listen(5000, () => {
+//   console.log('Server is running on port 5000');
+// }
+// );
