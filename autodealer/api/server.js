8ffdfd93a27980f0d59d4c818e735c4bf39c36d3
noothanpanthayi <noothankrishnan@outlook.com>
2026-03-01 08:52:52 -0600

    updates

--- autodealer/api/server.js
+++ autodealer/api/server.js
@@ -1,30 +1,17 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
 
+// Root route
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
 });
 
-// Add your API routes here
+// Test API route
 app.get('/api/test', (req, res) => {
   res.json({ success: true });
 });
 
-module.exports = serverless(app);
-
-// const express = require('express');
-// // const serverless = require('serverless-http');
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Hello from Express on Vercel!' });
-// });
-
-// app.listen(5000, () => {
-//   console.log('Server is running on port 5000');
-// }
-// );
+// JUST EXPORT THE APP - No serverless() wrapper needed
+module.exports = app;
